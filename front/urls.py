2df7b458e2b24a77c46cdf00f3a1d0f6e3416130
Chiranjit Bej <chiranjitb@spaatech.net>
2023-07-04 14:15:18 +0530

    url patterns

--- front/urls.py
+++ front/urls.py
@@ -4,11 +4,12 @@ from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('import-csv', views.importCsv, name='importCsv'),
-    
+
     path('', views.lineChart, name='lineChart'),
+
     path('scatter-chart', views.scatterChart, name='scatterChart'),
     path('getScatterChartData', views.getScatterChartData, name='getScatterChartData'),
-
+    
     path('line-chart', views.lineChart, name='lineChart'),
     path('getLineChartData', csrf_exempt(views.getLineChartData), name='getLineChartData')
 ]
